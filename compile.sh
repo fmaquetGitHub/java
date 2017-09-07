9303367ec8a8c75d60cc87718a44d4b9537f5571
FREDERIC MAQUET <frederic.maquet@ext.adeo.com>
2017-09-07 15:03:48 +0200

    [JAVA8] : ClassLoader et BootStrapClassLoader : modification compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 rm -rf classes/*/
-
 javac src/common/math/Calculatrice.java -d classes
 jar -cf lib/calculatrice.jar -C classes common
-
+rm -rf classes/*/
 javac -classpath lib/calculatrice.jar src/norsys/main/Main.java -d classes
