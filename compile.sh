402d83425d8836c2fdaf9630525021befaf937f5
FREDERIC MAQUET <frederic.maquet@ext.adeo.com>
2017-09-25 15:14:40 +0200

    [JAVA 8] : JAXB avec et sans --add-modules

--- compile.sh
+++ compile.sh
@@ -2,5 +2,11 @@
 
 rm -rf classes/*
 
+# Compilation sans modules
+echo "compilation sans add-modules"
 javac src/main/*.java -d classes
+
+echo ""
+echo "compilation avec add-modules"
+javac --add-modules java.xml.bind  src/main/*.java -d classes
 
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "Run sans add-modules"
 java -classpath classes main.JAXBExample
 
+echo ""
+echo "Run avec add-modules"
+java --add-modules java.xml.bind -classpath classes main.JAXBExample
